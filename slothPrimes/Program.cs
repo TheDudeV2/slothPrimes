7ab3fb528525cf4d38e52a18047ad6f907a70c9f
ectremblay <ectremblay@gmail.com>
2020-12-23 11:43:31 -0700

    Add project files.

---
+++ slothPrimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * PRIME NUMBERS
+ * Version 1 - naive approach
+ * ---------
+ * 1) Determine all prime numbers from 2 to 1,000,000
+ * 2) Display graph showing prime count for each 10,000 range
+ * 
+ * 
+ * Version 2 - OOP and Eresthosthenes
+ * ---------
+ * 1) Determine all prime numbers from 2 to 10,000,000,000
+ * 2) Display graph showing prime count for each 100,000 range
+ *  
+ * 
+ */
+
+namespace slothPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
